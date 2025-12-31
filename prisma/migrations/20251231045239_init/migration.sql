ace06e6c61ed06eef3e2b37f4e2b3c59b4b522fe
Matheus Bruno Almeida <manancialsup@hotmail.com>
2025-12-31 01:54:14 -0300

    generate schema prisma

--- prisma/migrations/20251231045239_init/migration.sql
+++ prisma/migrations/20251231045239_init/migration.sql
@@ -1,25 +1,31 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "phone" TEXT,
     "city" TEXT NOT NULL,
     "state" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "structure" TEXT NOT NULL,
+    "otherStructure" TEXT,
     "region" TEXT,
     "role" TEXT NOT NULL DEFAULT 'USER',
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "ReadingProgress" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "dayNumber" INTEGER NOT NULL,
     "isCompleted" BOOLEAN NOT NULL DEFAULT false,
-    "completedAt" DATETIME,
-    CONSTRAINT "ReadingProgress_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "completedAt" TIMESTAMP(3),
+
+    CONSTRAINT "ReadingProgress_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -27,3 +33,6 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "ReadingProgress_userId_dayNumber_key" ON "ReadingProgress"("userId", "dayNumber");
+
+-- AddForeignKey
+ALTER TABLE "ReadingProgress" ADD CONSTRAINT "ReadingProgress_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
